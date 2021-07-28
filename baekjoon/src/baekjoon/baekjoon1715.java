22508bfa076f6410d205bbb6eba55655f35e9558
yudonggeun <67232422+yudonggeun@users.noreply.github.com>
2021-07-28 14:41:08 +0900

    캠핑 4796번 문제 파일 추가

---
+++ baekjoon/src/baekjoon/baekjoon1715.java
@@ -0,0 +1,27 @@
+package baekjoon;
+import java.io.*;
+import java.util.*;
+
+public class baekjoon1715 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Long> queue = new PriorityQueue<Long>();
+		for(int i = 0; i< n ;i++) {
+			queue.add(Long.parseLong(br.readLine()));
+		}
+		Long answer = 0l;
+		while(queue.size() != 1) {
+			Long next = queue.poll()+queue.poll();
+			answer += next;
+			queue.add(next);
+		}
+		bw.write(answer+"");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
