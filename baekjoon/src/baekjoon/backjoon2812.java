3a809c84322a34ae80a09886529003627a0b352c
yudonggeun <67232422+yudonggeun@users.noreply.github.com>
2021-08-20 16:11:11 +0900

    동방 프로젝트(small) 14594번 문제 파일 추가

--- baekjoon/src/baekjoon/backjoon2812.java
+++ baekjoon/src/baekjoon/backjoon2812.java
@@ -4,34 +4,24 @@ import java.io.*;
 
 public class backjoon2812 {
 	
-	public static char[] oneMinus(LinkedList<Character> num, int delete) {
-		Stack<Character> pront = new Stack<Character>();
+	public static char[] getNum(LinkedList<Character> num, int delete) {
+		Deque<Character> pront = new LinkedList<Character>();
 		int goal = num.size() - delete;
-		char[] result = new char[num.size()-delete];
+		char[] result = new char[goal];
 		if(num.size() == 1)
 			return null;
 		
-		while(pront.size() < goal) {
-			if(delete == 0) {
-				while(!num.isEmpty()) {
-					pront.push(num.removeFirst());
-				}
-				break;
-			}
-			if(pront.isEmpty()) {
-				pront.push(num.removeFirst());
-			}
-			if(pront.peek() >= num.getFirst()) {
-				pront.push(num.removeFirst());
-			}
-			else if(pront.peek() < num.getFirst()) {
-				pront.pop();
+		while(!num.isEmpty()) {
+			char p = num.removeFirst();
+			while(!pront.isEmpty() && delete > 0 && pront.peekLast() < p) {
+				pront.removeLast();
 				delete--;
 			}
+			pront.add(p);
 		}
 		
-		for(int i = pront.size()-1 ; i> -1; i--) {
-			result[i] = pront.pop();
+		for(int i = 0; i< goal; i++) {
+			result[i] = pront.removeFirst();
 		}
 		
 		return result;
@@ -39,15 +29,18 @@ public class backjoon2812 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		String [] input = br.readLine().split(" ");
 		int N = Integer.parseInt(input[0]);
 		int K = Integer.parseInt(input[1]);
+		
 		LinkedList<Character> num = new LinkedList<Character>();
 		char [] numString = br.readLine().toCharArray();
+		
 		for(char n : numString) {
 			num.add(n);
 		}
-		bw.write(oneMinus(num, K));
+		bw.write(getNum(num, K));
 		bw.flush();
 		bw.close();
 		br.close();
