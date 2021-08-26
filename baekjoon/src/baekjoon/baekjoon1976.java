f2cc776757ff5ac56c3dd8bf3832f56f3c356d54
yudonggeun <67232422+yudonggeun@users.noreply.github.com>
2021-08-26 14:45:24 +0900

    여행가자 1976번 문제 파일 추가

---
+++ baekjoon/src/baekjoon/baekjoon1976.java
@@ -0,0 +1,50 @@
+package baekjoon;
+import java.io.*;
+
+public class baekjoon1976 {
+	static int [] city;
+	static int find(int index) {
+		return city[index] == index ? index : find(city[index]);
+	}
+	static void union(int x, int y) {
+		int pX = find(x);
+		int pY = find(y);
+		city[pX] = pY;
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int cityCount = Integer.parseInt(br.readLine());
+		int plan = Integer.parseInt(br.readLine());
+		city = new int[cityCount];
+		for(int i = 0; i< cityCount; i++) {
+			city[i] = i;
+		}
+		
+		for(int i = 0; i< cityCount; i++) {
+			String[] input = br.readLine().split(" ");
+			for(int j = 0; j < cityCount; j++) {
+				if(input[j].equals("1")) {
+					union(i, j);
+				}
+			}
+		}
+		
+		String [] input = br.readLine().split(" ");
+		int parent = find(Integer.parseInt(input[0])-1);
+		boolean isPossible = true;
+		
+		for(int i = 1; i< plan; i++) {
+			if(parent != find(Integer.parseInt(input[i])-1)) {
+				isPossible = false;
+				break;
+			}
+		}
+		
+		bw.write((isPossible ? "YES" : "NO"));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
