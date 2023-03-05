052663fc62c1c4cfeca3fce94bfd587840071d88
yudonggeun <ydong98@gmail.com>
2023-03-05 11:19:24 +0900

    23-03-05

---
+++ programmers/src/programmers/RentalRoom.java
@@ -0,0 +1,42 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/155651
+import java.util.StringTokenizer;
+
+class Solution {
+    public int solution(String[][] book_time) {
+
+        int[] sum = new int[24 * 60 + 10];
+
+        for (String[] time : book_time) {
+            Time start = new Time(time[0]);
+            Time end = new Time(time[1]);
+
+            sum[start.getIndex()] += 1;
+            sum[end.getIndex() + 10] -= 1;
+        }
+
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = sum[i] + sum[i - 1];
+        }
+
+        int max = 0;
+        for (int i : sum) {
+            max = Math.max(i, max);
+        }
+
+        return max;
+    }
+
+    class Time{
+        int hour, minute;
+
+        public Time(String times) {
+            StringTokenizer st = new StringTokenizer(times, ":");
+            hour = Integer.parseInt(st.nextToken());
+            minute = Integer.parseInt(st.nextToken());
+        }
+
+        public int getIndex(){
+            return hour * 60 + minute;
+        }
+    }
+}
