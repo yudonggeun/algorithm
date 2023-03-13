c46df4eed2c9fef49c0ab693f8098c3124bb40e1
yudonggeun <ydong98@gmail.com>
2023-03-13 23:08:43 +0900

    23-03-13

---
+++ programmers/src/programmers/Level 2/DotPrint.java
@@ -0,0 +1,26 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/142085
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(o -> -o));
+
+        int index = 0;
+
+        for (; index < enemy.length; index++) {
+            int target = enemy[index];
+            queue.add(target);
+            n -= target;
+
+            if(n < 0) {
+                if(k == 0) break;
+                k--;
+                n += queue.poll();
+            }
+        }
+
+        return index;
+    }
+}
