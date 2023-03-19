94a65dd5bcd4ee4b1507191ef8e14c3ca6679a91
yudonggeun <67232422+yudonggeun@users.noreply.github.com>
2023-03-19 18:08:52 +0900

    23-03-19

---
+++ programmers/src/programmers/Level 2/CutRollCake.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(int[] topping) {
+
+        int answer = 0;
+        info person1 = new info();
+        info person2 = new info();
+
+        for (int t : topping) person1.add(t);
+
+        for (int t : topping) {
+            person1.minus(t);
+            person2.add(t);
+
+            if(person1.getTypeCount() == person2.getTypeCount()){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    class info {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        public void add(int topping) {
+            map.putIfAbsent(topping, 0);
+            map.computeIfPresent(topping, (type, count) -> count + 1);
+        }
+
+        public void minus(int topping) {
+            map.computeIfPresent(topping, (type, count) -> count - 1);
+            if(map.get(topping).equals(0)) map.remove(topping);
+        }
+
+        public int getTypeCount(){
+            return map.keySet().size();
+        }
+    }
+}
