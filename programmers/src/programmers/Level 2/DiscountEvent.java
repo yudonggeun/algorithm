38195c6219e744677590d1d0d2715fde6f647054
yudonggeun <67232422+yudonggeun@users.noreply.github.com>
2023-03-23 22:09:55 +0900

    23-03-23

---
+++ programmers/src/programmers/Level 2/DiscountEvent.java
@@ -0,0 +1,50 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/131127
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+
+        Info 정현 = new Info();
+        Info mart = new Info();
+
+        for (int i = 0; i < want.length; i++) 정현.add(want[i], number[i]);
+        for (int i = 0; i < 9; i++) mart.add(discount[i]);
+
+        int count = 0;
+
+        for (int i = 9; i < discount.length; i++) {
+            mart.add(discount[i]);
+            if(isMatch(정현, mart)) count++;
+            mart.remove(discount[i - 9]);
+        }
+
+        return count;
+    }
+
+    public boolean isMatch(Info consumer, Info provider){
+        for (String product : consumer.info.keySet())
+            if (consumer.getCount(product) > provider.getCount(product)) return false;
+        return true;
+    }
+    class Info {
+        Map<String, Integer> info = new HashMap<>();
+
+        public void add(String product) {
+            info.putIfAbsent(product, 0);
+            info.computeIfPresent(product, (k, v) -> v + 1);
+        }
+
+        public void add(String product, Integer count) {
+            info.putIfAbsent(product, count);
+        }
+
+        public Integer getCount(String product) {
+            return info.getOrDefault(product, 0);
+        }
+
+        public void remove(String product) {
+            info.computeIfPresent(product, (k, v) -> Math.max(0, v - 1));
+        }
+    }
+}
