cc27f4f2bb615de2189b11bcbe3517930b8e0c35
yudonggeun <67232422+yudonggeun@users.noreply.github.com>
2023-03-27 20:21:31 +0900

    23-03-27

---
+++ programmers/src/programmers/Level 3/Move110.java
@@ -0,0 +1,35 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/77886
+class Solution {
+    public String[] solution(String[] s) {
+        String[] answer = new String[s.length];
+        for (int i = 0; i < s.length; i++) {
+            answer[i] = getString(s[i]);
+        }
+
+        return answer;
+    }
+
+    private String getString(String s) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder plus = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            sb.append(c);
+            if (sb.length() >= 3 && sb.charAt(sb.length() - 3) == '1' && sb.charAt(sb.length() - 2) == '1' && sb.charAt(sb.length() - 1) == '0') {
+                sb.delete(sb.length() - 3, sb.length());
+                plus.append("110");
+            }
+        }
+
+        if (plus.length() > 0) {
+            int index = sb.lastIndexOf("0");
+            if (index != -1) {
+                sb.insert(index + 1, plus);
+            } else {
+                sb.insert(0, plus);
+            }
+        }
+        return sb.toString();
+    }
+}
