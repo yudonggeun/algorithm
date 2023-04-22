0780e4ec6de3e103157d0c8a022820204920966f
yudonggeun <ydong98@gmail.com>
2023-04-22 10:44:20 +0900

    23-04-22

---
+++ programmers/src/programmers/Level 1/Coke.java
@@ -0,0 +1,14 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/132267
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while(n >= a){
+            int left = n % a;
+            int coke = n / a * b;
+            n = coke + left;
+            answer += coke;
+        }
+        return answer;
+    }
+}
